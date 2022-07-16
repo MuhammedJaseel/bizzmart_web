2f99b2a9660b4d55bd64f2fa8e936820784fdc0d
jaseelPerfectz <jaseelmanmuli@gmail.com>
2022-07-16 10:53:29 +0530

    Ui finel tuchup started

--- src/widget/home_cashbank.js
+++ src/widget/home_cashbank.js
@@ -1,7 +1,8 @@
-import React, { Component, StrictMode } from "react";
+import React, { Component } from "react";
 import { Header1, Header4, HeaderButtens1, PaymentCard1 } from "./widget";
 import { homeCashbankTitles } from "../module/home_cashbank";
 import { homeCashbankPopupTitles } from "../module/home_cashbank";
+import { WidgetPopUp1 } from "./widget_popup";
 import "../style/hcb.css";
 
 const tit = homeCashbankTitles;
@@ -10,18 +11,14 @@ const popTit = homeCashbankPopupTitles;
 export default class HomeCashbank extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      page: 0,
-      popUp: null,
-    };
+    this.state = { page: 0, popup: null };
   }
   render() {
     const state = this.state;
     const setState = (v) => this.setState(v);
     const { page } = state;
-
     const bodyRBody = {
-      makeAdd: () => setState({ popUp: 0 }),
+      makeAdd: () => setState({ popup: 0 }),
       title: "+ New Account",
       drowelList: [
         { title: "Add Account", fun: () => setState({ popUp: 0 }) },
@@ -32,6 +29,7 @@ export default class HomeCashbank extends Component {
       ],
     };
     const bodyR = page === 0 ? <HeaderButtens1 props={bodyRBody} /> : null;
+
     return (
       <React.StrictMode>
         <Header1 title="CASH & BANK" bodyL="ACCOUNTS" bodyR={bodyR} />
@@ -72,76 +70,12 @@ function ChequeList({ state, setState }) {
 }
 
 function PopUpLayout({ state, setState }) {
-  const { popUp } = state;
-  if (popUp === null) return null;
-  return (
-    <div className="hcbD">
-      <div className="hcbDa">
-        <div className="hcbDb">
-          <div className="hcbDbA">{popTit[popUp].title}</div>
-          <div className="hcbDbB" onClick={() => setState({ popUp: null })} />
-        </div>
-        <div className="hcbDc">{popTit[popUp].desc}</div>
-        <PopUpAddAcount state={state} setState={setState} />
-        <div className="hcbDd">
-          <div className="hcbDdA">Errors on page</div>
-          <div className="hcbDdB">
-            <div className="hcbDdBa" onClick={() => setState({ popUp: null })}>
-              CANCEL
-            </div>
-            <div className="hcbDdBb">SAVE</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-function PopUpAddAcount({ state, setState }) {
-  return (
-    <StrictMode>
-      <div className="hcbEc">
-        <div className="hcbEcA">
-          <div className="hcbEcAa">Account Type*</div>
-          <select className="hcbEcAb">
-            <option>Select account type</option>
-          </select>
-        </div>
-        <div className="hcbEcA">
-          <div className="hcbEcAa">Select Bank*</div>
-          <select className="hcbEcAb">
-            <option>Select your bank</option>
-          </select>
-        </div>
-      </div>
-      <div className="hcbEc">
-        <div className="hcbEcA">
-          <div className="hcbEcAa">Account Holder*</div>
-          <input className="hcbEcAb" />
-        </div>
-        <div className="hcbEcA">
-          <div className="hcbEcAa">Account Number*</div>
-          <input className="hcbEcAb" />
-        </div>
-      </div>
-      <div className="hcbEc">
-        <div className="hcbEcB">
-          <div className="hcbEcBa">Branch*</div>
-          <input className="hcbEcAb" />
-        </div>
-        <div className="hcbEcB">
-          <div className="hcbEcBa">IBAN/IFSC*</div>
-          <input className="hcbEcAb" />
-        </div>
-        <div />
-        <div className="hcbEcB">
-          <div className="hcbEcBa">Account Balance</div>
-          <input className="hcbEcAb" />
-        </div>
-        <div className="hcbEcB">
-          <div className="hcbEcBa">As On</div>
-          <input className="hcbEcAb" />
-        </div>
-      </div>
-    </StrictMode>
-  );
+  const { popup } = state;
+  if (popup === null) return null;
+  const popupProps1 = {
+    close: () => setState({ popup: null }),
+    title: popTit[popup]?.title,
+    desc: popTit[popup]?.desc,
+  };
+  return <WidgetPopUp1 props={popupProps1} />;
 }
